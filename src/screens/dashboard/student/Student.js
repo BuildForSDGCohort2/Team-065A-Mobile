40bf8d0d3f37a5dff7fb21b3c5cc78dfc0fa1b17
peter2c <peter2c@yahoo.com>
2020-09-30 19:54:09 +0100

    stu dashboard

--- src/screens/dashboard/student/Student.js
+++ src/screens/dashboard/student/Student.js
@@ -1,19 +1,50 @@
 import React, { Component } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Dimensions } from "react-native";
 import { inject, observer } from "mobx-react";
-import { Header, Left, Button, Icon, Title, Body, Right } from "native-base";
+import {
+  Container,
+  Header,
+  Left,
+  Button,
+  Title,
+  Body,
+  Right,
+  Footer,
+  FooterTab,
+  Content,
+  Badge,
+  Icon,
+} from "native-base";
 import {
   MaterialIcons,
   MaterialCommunityIcons,
 } from "react-native-vector-icons";
+import { BottomNavigation } from "react-native-paper";
+const { width, height } = Dimensions.get("window");
 
 @inject("store")
 @observer
 export default class Student extends Component {
-  state = { btnTopRight: false };
+  state = { btnTopRight: false, bgActive: "", iconActive: "" };
+  goToProfilepage = () => {
+    this.setState({ bgActive: "#09822E", iconActive: "#fff" }, () => {
+      console.log("done.");
+      // do something here
+    });
+  };
+  goToSubjectpage = () => {
+    this.setState({ bgActive: "#09822E", iconActive: "#fff" }, () => {
+      // do something here
+    });
+  };
+  goToNotificationpage = () => {
+    this.setState({ bgActive: "#09822E", iconActive: "#fff" }, () => {
+      // do something here
+    });
+  };
   render() {
     return (
-      <View style={styles.container}>
+      <Container>
         <Header
           iosBarStyle="light-content"
           androidStatusBarColor={"#09822E"}
@@ -47,7 +78,40 @@ export default class Student extends Component {
             )}
           </Right>
         </Header>
-      </View>
+
+        <Content />
+        <Footer>
+          <FooterTab style={{ backgroundColor: "#86bc42" }}>
+            <Button onPress={this.goToProfilepage} vertical>
+              <Icon name="person" style={{ color: this.state.iconActive }} />
+              <Text>Profile</Text>
+            </Button>
+            <Button style={{ backgroundColor: this.state.bgActive }} vertical>
+              <Icon
+                style={{ color: this.state.iconActive }}
+                name="ios-clipboard"
+              />
+              <Text>Subjects</Text>
+            </Button>
+
+            {/* <Button vertical>
+              <Icon name="person" />
+              <Text>Contact</Text>
+            </Button> */}
+
+            <Button badge vertical>
+              <Badge>
+                <Text>{2}</Text>
+              </Badge>
+              <Icon
+                style={{ color: this.state.iconActive }}
+                name="ios-notifications"
+              />
+              <Text>Notification</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
@@ -55,3 +119,6 @@ export default class Student extends Component {
 const styles = StyleSheet.create({
   container: { backgroundColor: "#fff" },
 });
+
+// http://discuss.nativebase.io/t/where-to-get-the-list-of-icon-names-used-in-native-base/37/18
+// icons native base
